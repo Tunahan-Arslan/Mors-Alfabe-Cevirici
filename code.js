7d6013225d0cbfca7d2c6c97233bdf93968f9c7c
Tunahan Arslan <101629477+Tunahan-Arslan@users.noreply.github.com>
2023-08-24 17:12:05 +0300

    Add files via upload

--- code.js
+++ code.js
@@ -8,11 +8,22 @@ const morseAlfabesi = {
     '4': '....-', '5': '.....','6': '-....','7': '--...','8': '---..',
     '9': '----.',
   };
+const morseAlfabesi2 = {
+    ".-":'A',     "-...":'B',  "-.-.":'C',   "-.-..":'Ç',  "-..":'D',    ".":'E', 
+    "..-.":'F',   "--.":'G',   "--.":'Ğ',    "....":'H',   "..":'I',     "..":'İ',    
+    ".---":'J',   "-.-":'K',   ".-..":'L',   "--":'M',     "-.":'N',     "---":'O',
+    "---.":'Ö',   ".--.":'P',  ".-.":'R',    "...":'S',    "...-":'Ş',   "-":'T',
+    "..-":'U',    "..--":'Ü',  "...-":'V',   "-.--":'Y',   "--..":'Z',
+    "-----":'0',  ".----":'1', "..---":'2',  "...--":'3',  "....-":'4',  ".....":'5',
+    "-....":'6',  "--...":'7',  "---..":'8', "----.":'9',
+  };
+
 var turkceYazi=document.getElementById("turkceYazi");
 var morsYazi=document.getElementById("morsYazi");
 var sonuc=document.getElementById("sonuc");
 
 document.getElementById("yaziyiMorsa").style.display="none";
+document.getElementById("morsuYaziya").style.display="none";
 
 var aktiflikYM=false;
 function yaziyiMorsaMenu(){
@@ -26,13 +37,27 @@ function yaziyiMorsaMenu(){
         aktiflikYM=false;
     }
 }
-function hesapla(){
-    var trMetin=turkceYazi.value.toUpperCase();
-    let morsYazi='';
+var aktiflikMY=false;
+function morsuYaziyaMenu(){
+    var ekranaGetir=document.getElementById("morsuYaziya");
+    if(aktiflikMY==false){
+        ekranaGetir.style.display="block";
+        aktiflikMY=true;
+    }
+    else if(aktiflikMY==true){
+        ekranaGetir.style.display="none";
+        aktiflikMY=false;
+    }
+}
+function hesapla(morsKod){
+    //Yazıyı Morsa çeviren kodlar aşağıda;
+    if(turkceYazi.value!="" && morsYazi.value==""){
+        var trMetin=turkceYazi.value.toUpperCase();
+        let morsYazi='';
         for(var i=0;i<trMetin.length;i++){
             const karakter=trMetin[i];
         
-        if(karakter===' '){
+            if(karakter===' '){
             morsYazi+=' / ';
         }
         else if(morseAlfabesi[karakter]){
@@ -40,5 +65,41 @@ function hesapla(){
         }
     }
     sonuc.innerText=morsYazi;
+    }
+    //Morsu Yazıya çeviren kodlar aşağıda;
+    else if(morsYazi.value!="" && turkceYazi.value==""){
+    //
+    var morsMetin = morsYazi.value;
+    const cevrilmisMetin = morsuTurkceyeCevir(morsMetin);
+    sonuc.innerText = cevrilmisMetin;
+    //
+    }
+    else{
+        alert("İkisini birden kullanmayın veya boş bırakmayın!");
+        turkceYazi.value="";
+        morsYazi.value="";
+    }
+}
+//
+function morsuTurkceyeCevir(morsKod) {
+    const morsKelimeler = morsKod.split(' / ');
+    let cevrilmisMetin = '';
 
+    for (const morsKelime of morsKelimeler) {
+        const morsHarfler = morsKelime.split(' ');
+        let cevrilmisKelime = '';
+
+        for (const morsHarf of morsHarfler) {
+            if (morseAlfabesi2[morsHarf]) {
+                cevrilmisKelime += morseAlfabesi2[morsHarf];
+            } else {
+                // Tanınmayan karakterleri işle
+                cevrilmisKelime += '?';
+            }
+        }
+
+        cevrilmisMetin += cevrilmisKelime + ' ';
+    }
+
+    return cevrilmisMetin.trim();
 }
